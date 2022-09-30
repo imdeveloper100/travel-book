e35d5e73fc6df74352a710a6077886ff3c000fbb
imdeveloper100 <imdeveloper100@gmail.com>
2022-10-01 00:53:53 +0500

    Chat Component added

---
+++ components/chat/index.js
@@ -0,0 +1,7 @@
+import Styles from "./chat.module.css";
+import Typography from "@mui/material/Typography";
+export default function Chat() {
+  return (<>
+    <Typography>I am Chat Section</Typography>
+  </>);
+}
---
+++ pages/chat/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import Chat from '../../components/chat';
+export default function ChatPage() {
+    return ( <>
+    <Chat />
+    </> );
+}
